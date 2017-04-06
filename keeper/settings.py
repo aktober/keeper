0865a9077da524cd975f6e1e6ca424c6875ad22b
Anna Popovychenko <a.popovychenko@gmail.com>
2017-04-07 00:45:54 +0300

    added custom error pages

--- keeper/settings.py
+++ keeper/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'keeper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'keeper/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
